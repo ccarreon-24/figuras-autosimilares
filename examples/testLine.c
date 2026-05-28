f29e3a639dd49b31af4e7482c5528f9a95df23f8
Carlos Carreon <ccarreon@unsa.edu.pe>
2026-05-27 21:40:35 -0500

    angulo 20

--- examples/testLine.c
+++ examples/testLine.c
@@ -4,13 +4,13 @@ void fractalTree(Turtle *t, int length, int depth){
   if(depth == 0 || length < 6)
 		return ;
 	turtleForward(t, length);
-	turtleLeft(t, 30);
+	turtleLeft(t, 20);
 	fractalTree(t, length/2, depth - 1);
 
-	turtleRight(t, 60);
+	turtleRight(t, 40);
 	fractalTree(t, length/2, depth - 1);
 
-	turtleLeft(t, 30);
+	turtleLeft(t, 20);
 	turtleBackward(t, length);
 }
 
